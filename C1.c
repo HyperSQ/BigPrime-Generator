eeeb75833c3aec78e6f253c110c647be81cd0b12
HyperSQ <60601112+HyperSQ@users.noreply.github.com>
2024-10-06 13:03:38 +0800

    Update C1.c

--- C1.c
+++ C1.c
@@ -52,6 +52,7 @@ ull find_num(char c){
     if('0'<=c&&c<='9') return c-'0';
     if('a'<=c&&c<='f') return c-'a'+10;
     if('A'<=c&&c<='F') return c-'A'+10;
+    return 0;
 }
 
 void trans_x(Int *A,char ch[]){//16进制转化
